c428831526d2a5083a2b1f5880d2e4f5ddc57f64
dheerajgupta03 <dheerajgupta72487@gmail.com>
2025-06-26 11:17:03 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs ,not browser
+
+console.log(3 +3);
+
+console.log("Dheeraj")
+
+
+let name = "dheeraj"
+let age =17
+let isLoggedIn = false
+
+//namber= => 2 to power 53 range hai 
+//bigint trading me stock market hai genrra

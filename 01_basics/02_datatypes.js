0680ca5ddafbaed1fe023054258177169f1e1983
dheerajgupta03 <dheerajgupta72487@gmail.com>
2025-06-26 11:29:57 +0000

    container

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -12,4 +12,21 @@ let age =17
 let isLoggedIn = false
 
 //namber= => 2 to power 53 range hai 
-//bigint trading me stock market hai genrra
+//bigint trading me stock market hai genrrally use hota
+
+//string => ""
+
+//boolean => true/false
+
+//null => standalone value mtlb empty hai ,intensionally btya gya
+
+//undefined =>  /value hii assigned nii kia hai
+
+//symbol => unique 
+
+
+//object 
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); //undefined
